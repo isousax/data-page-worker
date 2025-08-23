6480849b2dc79bd15d756a3bc2cec2b53d76139c
Italo Williams <italowilliams2020@gmail.com>
2025-08-22 22:05:45 -0300

    alterado cros

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ export default {
 				headers: {
 					"Access-Control-Allow-Origin": "*",
 					"Access-Control-Allow-Methods": "POST, OPTIONS, GET",
-					"Access-Control-Allow-Headers": "Content-Type"
+					"Access-Control-Allow-Headers": "Content-Type, Authorization"
 				}
 			});
 		}
